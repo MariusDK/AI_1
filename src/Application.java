61dcb7c17aadd6f31fa363f882120820339e0cb1
ionutincau <ionut.incau@gmail.com>
2017-03-06 03:16:08 +0200

    Add p2 and p3

--- src/Application.java
+++ src/Application.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by Incau Ionut on 05-Mar-17.
  * Contact: ionut.incau@gmail.com
@@ -22,5 +26,50 @@ public class Application {
             }
         }
         return longest_word;
+    }
+
+
+    /**
+     * Să se determine frecvența de apariție a cuvintelor într-un text.
+     */
+    public static HashMap p2(String in) {
+        String[] splited = in.split("\\s+");
+        HashMap counter = new HashMap(splited.length);
+        for (String word : splited) {
+            if (counter.containsKey(word)) {
+                counter.put(word, Integer.parseInt(counter.get(word).toString()) + 1);
+            }
+            else {
+                counter.put(word, 1);
+            }
+        }
+        return counter;
+    }
+
+    /**
+     * Să se determine cel mai frecvent cuvânt într-un text.
+     */
+    public static Map.Entry p3(String in) {
+        String[] splited = in.split("\\s+");
+        HashMap counter = new HashMap(splited.length);
+        for (String word : splited) {
+            if (counter.containsKey(word)) {
+                counter.put(word, Integer.parseInt(counter.get(word).toString()) + 1);
+            }
+            else {
+                counter.put(word, 1);
+            }
+        }
+
+        Map.Entry maxEntry = null;
+        Iterator it = counter.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (maxEntry == null || Integer.parseInt(maxEntry.getValue().toString()) < Integer.parseInt(pair.getValue().toString())) {
+                maxEntry = pair;
+            }
+            it.remove();
+        }
+        return maxEntry;
     }
 }
--- tests/ApplicationTest.java
+++ tests/ApplicationTest.java
@@ -1,4 +1,9 @@
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
@@ -29,4 +34,54 @@ public class ApplicationTest {
         assertEquals(longest_word4, "Studenta");
     }
 
+    @Test
+    public void p2() throws Exception {
+        String in = "zero unu doi trei unu patru doi unu cinci";
+        HashMap counter = Application.p2(in);
+        assertTrue(Integer.parseInt(counter.get("zero").toString()) == 1);
+        assertTrue(Integer.parseInt(counter.get("unu").toString()) == 3);
+        assertTrue(Integer.parseInt(counter.get("doi").toString()) == 2);
+        assertTrue(Integer.parseInt(counter.get("trei").toString()) == 1);
+        assertTrue(Integer.parseInt(counter.get("patru").toString()) == 1);
+        assertTrue(Integer.parseInt(counter.get("cinci").toString()) == 1);
+        Iterator it = counter.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+            it.remove();
+        }
+
+        String in2 = "java";
+        HashMap counter2 = Application.p2(in2);
+        assertTrue(Integer.parseInt(counter2.get("java").toString()) == 1);
+
+        String in3 = "c++ c++";
+        HashMap counter3 = Application.p2(in3);
+        assertTrue(Integer.parseInt(counter3.get("c++").toString()) == 2);
+
+        String in4 = "c++ c++";
+        HashMap counter4 = Application.p2(in4);
+        assertFalse(Integer.parseInt(counter4.get("c++").toString()) == 1);
+
+        String in5 = "java";
+        HashMap counter5 = Application.p2(in5);
+        assertTrue(counter5.get("c++") == null);
+    }
+
+    @Test
+    public void p3() throws Exception {
+        String in = "zero unu doi trei unu patru doi unu cinci";
+        Map.Entry maxEntry = Application.p3(in);
+        assertTrue(Integer.parseInt(maxEntry.getValue().toString()) == 3);
+
+        assertFalse(Integer.parseInt(maxEntry.getValue().toString()) == 2);
+
+        assertFalse(maxEntry.getKey().equals("doi"));
+
+        String in2 = "c++ c++";
+        Map.Entry maxEntry2 = Application.p3(in2);
+        assertTrue(Integer.parseInt(maxEntry2.getValue().toString()) == 2);
+
+        assertTrue(maxEntry.getKey().equals("c++"));
+    }
 }
